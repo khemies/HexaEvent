b7616b5783f4a06675ab05cc5f8f0db5589bd85c
yahiachames <yahiachames@gmail.com>
2023-05-17 11:36:28 +0100

    update

--- controller/formCmptApis.js
+++
@@ -1,9 +0,0 @@
-import api from "./ApiConfig";
-
-export const getDonMethods = () => api.get("donation-methods/");
-export const getDonTypes = () => api.get("donation-types/");
-export const getDonSources = () => api.get("donation-sources/");
-export const acceptDon = (values) =>
-  api.post("donation-requests/validate", values);
-export const refuseDon = (values) =>
-  api.post("donation-requests/refuse", values);
--- controller/requestsApis.js
+++
@@ -1,7 +0,0 @@
-import api from "./ApiConfig";
-
-export const get_requests = (page, search, size) => {
-  return api.get(
-    `all-donations-request-list?page=${page}&size=${size}&search=${search}`
-  );
-};
